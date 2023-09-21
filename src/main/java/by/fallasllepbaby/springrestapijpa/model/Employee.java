5c60e8cf97cdcecf50443e4403889aa5ac31b0d3
Laparevich Daniil <danik.laparevich@bk.ru>
2023-09-21 11:01:11 +0300

    add: JPA Entity Employee

--- src/main/java/by/fallasllepbaby/springrestapijpa/model/Employee.java
+++ src/main/java/by/fallasllepbaby/springrestapijpa/model/Employee.java
@@ -2,6 +2,7 @@ package by.fallasllepbaby.springrestapijpa.model;
 
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonAnySetter;
+import jakarta.persistence.*;
 import lombok.ToString;
 import lombok.Setter;
 import lombok.Getter;
@@ -9,10 +10,28 @@ import lombok.Getter;
 @Setter
 @Getter
 @ToString
+@Entity
+@Table(name = "tbl_employee")
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "age")
     private Long age;
+
+    @Column(name = "location")
     private String location;
+
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "department")
     private String department;
+
 }
